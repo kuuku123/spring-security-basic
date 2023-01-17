44ffed7650257461d77b22b39479af703b5f8dc5
dohyun_lim <dohyun_lim@tmax.co.kr>
2023-01-17 10:19:22 +0900

    fixed lazy loading no session issue

--- src/main/java/com/example/corespringsecurity/security/service/FormUserDetailsService.java
+++ src/main/java/com/example/corespringsecurity/security/service/FormUserDetailsService.java
@@ -9,6 +9,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,6 +22,7 @@ public class FormUserDetailsService implements UserDetailsService {
     UserRepository userRepository;
 
     @Override
+    @Transactional(readOnly = true)
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
         Account account = userRepository.findByUsername(username);
